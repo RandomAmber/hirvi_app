253d013e211b595a5f4c99c5eb38d99d691eff46
victoria tsvetkova <randambery@gmail.com>
2023-10-30 19:11:49 +0200

    Alchemy game fix

--- src/pages/Games/Alchemy/Alchemy.js
+++ src/pages/Games/Alchemy/Alchemy.js
@@ -2,13 +2,27 @@ import { useState, useEffect } from "react"
 import { Draggable, Droppable, DragDropContext } from "react-beautiful-dnd";
 import './Alchemy.css';
 import wordData from "./WordData.json"
+import Button from "../Button";
+import Leaderboard from "../Leaderboard";
 
 const dataArray = Object.values(wordData)
+
+
 //Select a random data list
-
-
 const randomIndex = Math.floor(Math.random() * dataArray.length);
 const randomData = dataArray[randomIndex];
+
+//Shuffle the data:
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+shuffleArray(randomData);
+
+
 
 const WordHolders = {
   WordHolder1: {
@@ -77,6 +91,12 @@ const wordsPerRow = 4
 
 
 function Alchemy() {
+  //test buttons:
+
+  const rules = () => {
+    alert("Button rules clicked!")
+  }
+
   const [areas, setAreas] = useState(WordHolders);
   const [item1_translation, setItem1Translation] = useState('');
   const [item2_translation, setItem2Translation] = useState('');
@@ -126,7 +146,7 @@ function Alchemy() {
         setItem1Translation(item1Translation);
         setItem2Translation(item2Translation);
         const index = areas.WordHolder1.items[0].concats.indexOf(combined)
-        if(index !== -1){
+        if (index !== -1) {
           const concat_translation = areas.WordHolder1.items[0].concats_translation[index];
           setConcatTranslation(concat_translation)
         }
@@ -161,100 +181,159 @@ function Alchemy() {
 
   return (
     <div className="mainContainer">
-      <div className="leftArea">
-        <p>rules</p>
-        <p>leaderboard</p>
+      <div className="nameContainer">
+        <h1>Word Alchemy</h1>
       </div>
+      <div className="areasContainer">
+        <div className="leftArea">
+        <Button className="button" label="rules" onClick={rules} />
+          
+          <Leaderboard game_id={2} limit={5} />
+          
 
-      <div className="centreContainer">
-        <h1 className="heading">Word Alchemy</h1>
-        <div className="gameContainer">
+        </div>
 
-          <div className="gameArea">
-            <DragDropContext onDragEnd={(result) => onDragEnd(result, areas, setAreas)}>
-              <div className="heading">
-                {matchesConcats && <p>Such word exists! Press the button to make a new one:</p>
-                  && <button onClick={clearWordHolders}>Clear WordHolders</button>
-                }
-                {/* /* Check if WordArea is empty */}
-                {areas.WordArea.items.length === 0 && (
-                  <button onClick={() => window.location.reload()}>Reload Page</button>
-                )}
+        <div className="centreContainer">
+          <div className="gameContainer">
 
-
-              </div>
-
-
-              <div className="wordHolders">
-                {/* Render WordHolder1 */}
-                <div>
-                  {matchesConcats &&  item1_translation }
-                  <div style={{ margin: 8 }}>
-                    <Droppable droppableId="WordHolder1">
-                      {(provided, snapshot) => (
-                        <div className="wordHolder"
-                          {...provided.droppableProps}
-                          ref={provided.innerRef}
-                          style={{
-                            background: snapshot.isDraggingOver ? "pink" : "blue", //wordHolder 1 colors
-                          }}
-                        >
-                          {areas.WordHolder1.items.map((item, index) => (
-                            <Draggable key={item.id} draggableId={item.id} index={index}>
-                              {(provided, snapshot) => (
-                                <div className="wordInWordHolder"
-                                  ref={provided.innerRef}
-                                  {...provided.draggableProps}
-                                  {...provided.dragHandleProps}
-                                  style={{
-                                    backgroundColor: snapshot.isDragging
-                                      ? "red"
-                                      : "blue",
-                                    ...provided.draggableProps.style,
-                                  }}
-                                >
-                                  {item.content}
-                                </div>
-                              )}
-                            </Draggable>
-                          ))}
-                          {provided.placeholder}
-                        </div>
-                      )}
-                    </Droppable>
-                  </div>
+            <div className="gameArea">
+              <DragDropContext onDragEnd={(result) => onDragEnd(result, areas, setAreas)}>
+                <div className="heading">
+                  {matchesConcats &&  areas.WordArea.items.length != 0 && <p>Such word exists! Press the button to make a new one:</p>
+                    && <button className="button" onClick={clearWordHolders}>Coin a new word</button>
+                  }
                 </div>
 
-                <p>+</p>
-
-                
                 <div className="wordHolders">
-                  {/* Render WordHolder2 */}
+                  {/* Render WordHolder1 */}
                   <div>
-                  {matchesConcats &&  item2_translation }
-                  
-                
+                    {matchesConcats && item1_translation}
+                    <div style={{ margin: 8 }}>
+                      <Droppable droppableId="WordHolder1">
+                        {(provided, snapshot) => (
+                          <div className="wordHolder"
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                            style={{
+                              background: snapshot.isDraggingOver ? "#76778E" : "#E6EAF2" , //wordHolder 1 colors
+                            }}
+                          >
+                            {areas.WordHolder1.items.map((item, index) => (
+                              <Draggable key={item.id} draggableId={item.id} index={index}>
+                                {(provided, snapshot) => (
+                                  <div className="wordInWordHolder"
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                    style={{
+                      
+                                      ...provided.draggableProps.style,
+                                    }}
+                                  >
+                                    {item.content}
+                                  </div>
+                                )}
+                              </Draggable>
+                            ))}
+                            {provided.placeholder}
+                          </div>
+                        )}
+                      </Droppable>
+                    </div>
+                  </div>
+
+                  <p>+</p>
+
+
+                  <div className="wordHolders">
+                    {/* Render WordHolder2 */}
+                    <div>
+                      {matchesConcats && item2_translation}
+
+
+                      <div style={{ margin: 8 }}>
+                        <Droppable droppableId="WordHolder2">
+                          {(provided, snapshot) => (
+                            <div className="wordHolder"
+                              {...provided.droppableProps}
+                              ref={provided.innerRef}
+                              style={{
+                                background: snapshot.isDraggingOver ? "#76778E" : "#E6EAF2", // wordHolder2 colors
+                              }}
+                            >
+                              {areas.WordHolder2.items.map((item, index) => (
+                                <Draggable key={item.id} draggableId={item.id} index={index}>
+                                  {(provided, snapshot) => (
+                                    <div className="wordInWordHolder"
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      style={{
+
+                                        ...provided.draggableProps.style,
+                                      }}
+                                    >
+                                      {item.content}
+                                    </div>
+                                  )}
+                                </Draggable>
+                              ))}
+                              {provided.placeholder}
+                            </div>
+                          )}
+                        </Droppable>
+
+
+                      </div>
+                    </div>
+
+
+                  </div>
+
+                  <p>=</p>
+                  {/* answerholder  */}
+                  <div className="answerHolder" >
+                    {matchesConcats && concat_translation}
+                    <div className="answerHolderInner"
+                    >
+                      {combinedWord}
+                    </div>
+
+                  </div>
+
+
+                </div>
+                {/* Render WordArea */}
+                {areas.WordArea.items.length === 0 && (
+                  <div className="winnerWindow">
+                    <p>Good job!</p>
+                    <button className="button" onClick={() => window.location.reload()}>Reload Page</button>
+
+                  </div>
+                    
+                  )}
+                {areas.WordArea.items.length != 0 && (
+                  <div className="wordArea" >
+                  <p>Words to tinker with</p>
                   <div style={{ margin: 8 }}>
-                    <Droppable droppableId="WordHolder2">
+                    <Droppable droppableId="WordArea">
                       {(provided, snapshot) => (
-                        <div className="wordHolder"
+                        <div className="wordAreaInner"
                           {...provided.droppableProps}
                           ref={provided.innerRef}
                           style={{
-                            background: snapshot.isDraggingOver ? "lightblue" : "lightgrey", // wordHolder2 colors
-                          }}
+                            background: snapshot.isDraggingOver ? "#76778E" : "#E6EAF2"
+                          }} //wordArea colors
                         >
-                          {areas.WordHolder2.items.map((item, index) => (
+                          {areas.WordArea.items.map((item, index) => (
                             <Draggable key={item.id} draggableId={item.id} index={index}>
                               {(provided, snapshot) => (
-                                <div className="wordInWordHolder"
+                                <div className="wordInWordArea"
                                   ref={provided.innerRef}
                                   {...provided.draggableProps}
                                   {...provided.dragHandleProps}
                                   style={{
-                                    backgroundColor: snapshot.isDragging
-                                      ? "#263B4A"
-                                      : "#456C86",
+                                   
                                     ...provided.draggableProps.style,
                                   }}
                                 >
@@ -267,86 +346,40 @@ function Alchemy() {
                         </div>
                       )}
                     </Droppable>
-
-
                   </div>
-                  </div>
-
-
                 </div>
+                    
+                  )}
+                
+              </DragDropContext>
 
-                <p>=</p>
-                {/* answerholder  */}
-                <div className="answerHolder" >
-                  {matchesConcats &&  concat_translation }
-                  <div className="answerHolderWFT" 
-                  >
-                    {combinedWord}
-                  </div>
-
-                </div>
-
-
-              </div>
-              {/* Render WordArea */}
-              <div className="wordArea" >
-                <p>Words to tinker with</p>
-                <div style={{ margin: 8 }}>
-                  <Droppable droppableId="WordArea">
-                    {(provided, snapshot) => (
-                      <div className="wordAreaWTF"
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        style={{
-                          background: snapshot.isDraggingOver ? "lightblue" : "lightgrey"}} //wordArea colors
-                      >
-                        {areas.WordArea.items.map((item, index) => (
-                          <Draggable key={item.id} draggableId={item.id} index={index}>
-                            {(provided, snapshot) => (
-                              <div className="wordInWordArea"
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                                style={{
-                                  backgroundColor: snapshot.isDragging ? "blue" : "red", //word colors!
-                                  ...provided.draggableProps.style,
-                                }}
-                              >
-                                {item.content}
-                              </div>
-                            )}
-                          </Draggable>
-                        ))}
-                        {provided.placeholder}
-                      </div>
-                    )}
-                  </Droppable>
-                </div>
-              </div>
-            </DragDropContext>
+            </div>
 
           </div>
 
+
+
+
+
         </div>
-
-
-
-
-
-      </div>
-      <div className="rightArea">
-        <div className="scoring">
-          <span>Your score: {score} </span>
+        <div className="rightArea">
+          <div className="score_alchemy">
+            <h3>Your score: {score}</h3>
+            
+          </div>
+          <div className="dict">
+          <h3>Words you coined:</h3>
+          <ul>
+            {dictionary.map((word, index) => (
+              <li key={index}>{word}</li>
+            ))}
+          </ul>
+          </div>
         </div>
-        <div className="dict"></div>
-        <p>Words you coined:</p>
-        <ul>
-          {dictionary.map((word, index) => (
-            <li key={index}>{word}</li>
-          ))}
-        </ul>
       </div>
+
     </div>
+
 
   );
 }
